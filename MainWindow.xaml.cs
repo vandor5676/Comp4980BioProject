6e0dc79b24c4437966e9c10be865ca2e5eb2a8b2
Shane <steiner5676@gmial.com>
2020-10-28 13:32:25 -0700

    Implemented node propertys: topLetter, leftLetter, cameFrom
    each node now has 2 letters, and the node that it came from

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MoreLinq;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -32,18 +33,21 @@ namespace Comp4980BioProject
             public int B { get; set; }
             public int C { get; set; }
         }
+
+        //create matrix
         private void startButton_Click(object sender, RoutedEventArgs e)
         {
             //get sequences
-
             seq1 = seq1TextBox.Text.ToUpper();
             seq2 = seq2TextBox.Text.ToUpper();
+
+            //get alighnment type
+            bool localAlighnment = (bool)localAlignmentRB.IsChecked;
 
             lgp = -1;//linear gap penalty
 
             //Declare Grid List
             grid = new List<List<Node>>();
-            //List<Node> nodeList = new List<Node>();
 
             //first 2 blanks in grid
             grid.Add(new List<Node>());//new line
@@ -59,12 +63,16 @@ namespace Comp4980BioProject
             //Add the second lines blank and zero
             grid.Add(new List<Node>());//new line
             grid[1].Add(new Node("blank"));
-            grid[1].Add(new Node { value = 0 });
+            grid[1].Add(new Node { value = 0, topLetter = '-', leftLetter = '-' });
 
             //write first linear gap penalty line 
             foreach (char letter in seq1)
             {
-                grid[1].Add(new Node { value = grid[1].Last().value + lgp });//each linear gap penalty is the previus square + the penalty
+                //if local alignment make all negatives equal to zero
+                if (localAlighnment)
+                    grid[1].Add(new Node { value = 0, topLetter = letter, leftLetter = '-', cameFrom = grid[1].Last() });
+                else
+                    grid[1].Add(new Node { value = grid[1].Last().value + lgp, topLetter = letter, leftLetter = '-', cameFrom = grid[1].Last() });//each linear gap penalty is the previus square + the penalty
             }
 
             //add each line untill the end
@@ -74,36 +82,48 @@ namespace Comp4980BioProject
                 int previusLine = grid.IndexOf(grid.Last());
 
                 tempLine.Add(new Node { valLetter = letter });//add a letter to the start of the line
-                tempLine.Add(new Node { value = (grid.Last()[1].value + (lgp)) });//sets the vertical linear gap penalty boxes
+
+                //if local alignment make all negatives equal to zero //----sets the vertical linear gap penalty boxes
+                tempLine.Add((localAlighnment) ?
+                    (new Node { value = 0, leftLetter = letter, topLetter = '-', cameFrom = grid.Last()[1] }) :
+                    (new Node { value = (grid.Last()[1].value + (lgp)), leftLetter = letter, topLetter = '-', cameFrom = grid.Last()[1] }));
 
                 foreach (char letter2 in seq1)
                 {
                     int curentIndex = tempLine.IndexOf(tempLine.Last()) + 1;
-                    int left = tempLine[curentIndex - 1].value;
-                    int top = grid[previusLine][curentIndex].value;
-                    int diag = grid[previusLine][curentIndex - 1].value;
+                    Node left = tempLine[curentIndex - 1];
+                    Node top = grid[previusLine][curentIndex];
+                    Node diag = grid[previusLine][curentIndex - 1];
 
-                    tempLine.Add(CalculateScore(left, top, diag));
+                    //get best choice
+                    Node cameFrom = GetBestNode(left, top, diag, localAlighnment);
+                    tempLine.Add(new Node { value = cameFrom.calcScore, leftLetter = letter2, topLetter = letter, cameFrom = cameFrom });
                 }
                 grid.Add(tempLine);
             }
+            //--
             //display grid
             DisplayGrid();
 
         }
-
-        public Node CalculateScore(int leftVal, int topVal, int diagVal)
+        public void setLetter(int row, int col)
         {
-            int leftScore, topScore, diagScore;
-            leftScore = leftVal + lgp;
-            topScore = topVal + lgp;
-            diagScore = diagVal + 1; //subMatrixLookup();
 
-            //get the bigest score
-            int bigest = leftScore > topScore ? leftScore : topScore;
-            bigest = bigest > diagScore ? bigest : diagScore;
+        }
 
-            return new Node { value = bigest };
+        public Node GetBestNode(Node leftNode, Node topNode, Node diagNode, bool isLocalAlignment)
+        {
+            leftNode.calcScore = leftNode.value + lgp;
+            topNode.calcScore = topNode.value + lgp;
+            diagNode.calcScore = diagNode.value + 1; //subMatrixLookup();
+
+            List<Node> Nodes = new List<Node> { leftNode, topNode, diagNode };
+            var bestNode = Nodes.MaxBy(x => x.calcScore).First();
+
+            if (isLocalAlignment && bestNode.calcScore < 0)
+                bestNode.calcScore = 0;
+
+            return bestNode;
         }
 
         private int subMatrixLookup()
@@ -118,7 +138,7 @@ namespace Comp4980BioProject
             int cols = seq1.Length + 2;
             int rows = seq2.Length + 2;
             flowDoc.Width = cols * 30;
-            
+
 
             //add the rowGroup (holds the rows)
             table1.RowGroups.Add(new TableRowGroup());
@@ -129,7 +149,7 @@ namespace Comp4980BioProject
             for (int r = 0; r < rows; r++)
             {
                 TableRow tr = new TableRow();
-                
+
 
                 for (int c = 0; c < cols; c++)
                 {
@@ -140,7 +160,7 @@ namespace Comp4980BioProject
                     //tr.Cells[c]
                     if (c == 4 && r == 4)
                         tr.Cells[4].Background = Brushes.Red;
-                    
+
 
                 }
                 TableRowGroup trg = new TableRowGroup();
--- Node.cs
+++ Node.cs
@@ -8,27 +8,28 @@ namespace Comp4980BioProject
 {
     public class Node
     {
-        char topLetter { get; set; }
-        char leftLetter { get; set; }
-
-        public char valLetter { get; set; } = '0';
-        public int value;
         
-        Node left, top, diag;
+       public char topLetter { get; set; }
+       public char leftLetter { get; set; }
 
-        string description;
+        //for elements with letters
+        public char valLetter { get; set; } = '0';
+        // for elements with numbers
+        public int value;
 
-        public Node(int leftVal, int topVal, int diagVal)
-        {
-            int leftScore = leftVal;
-            int stopint = 0;
-        }
+
+        public Node cameFrom { get; set; } = null;
+
+        //used when calculating best option(left top diag)
+       public int calcScore { get; set; }
 
         //nullNode
         public Node()
         {
             this.value = 100;
         }
+
+        //create a blank node 
         public Node(string value)
         {
             if(value == "blank")
