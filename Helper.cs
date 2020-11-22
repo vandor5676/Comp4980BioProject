1713d83057cd8f57a5ede3ccbdbbe9ff2b77149f
Shane <steiner5676@gmial.com>
2020-11-22 12:14:48 -0800

    Hooked up PAM and BLOSUM

--- Helper.cs
+++ Helper.cs
@@ -34,5 +34,40 @@ namespace Comp4980BioProject
             mw.LinearGapPenaltyTextBox.Text = num.ToString();
             return num;
         }
+
+        public static int GetMatrixNumber(MainWindow mainWindow, char matrixType)
+        {
+            if(matrixType == 'p')
+            {
+                if(mainWindow.PAM50Checkbox.IsChecked == true)
+                {
+                    return 50;
+                }
+                else if(mainWindow.PAM150Checkbox.IsChecked == true)
+                {
+                    return 150;
+                }
+                else if(mainWindow.PAM250Checkbox.IsChecked == true)
+                {
+                    return 250;
+                }
+            }
+            else
+            {
+                if (mainWindow.blosum62Checkbox.IsChecked == true)
+                {
+                    return 62;
+                }
+                else if (mainWindow.blosum80Checkbox.IsChecked == true)
+                {
+                    return 80;
+                }
+                else if (mainWindow.blosum90Checkbox.IsChecked == true)
+                {
+                    return 90;
+                }
+            }
+            return -1;
+        }
     }
 }
